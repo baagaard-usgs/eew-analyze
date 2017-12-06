35b92ba6af6cac51de1af826f7ccd946dcbfc363
Brad Aagaard <baagaard@usgs.gov>
2017-12-06 12:54:33 -0800

    Started work on implementing parsing of XML DM log.

--- shakealert.py
+++ shakealert.py
@@ -218,9 +218,57 @@ class DMLogXML(object):
         :param lines: Lines from DM log file.
         :returns: Numpy structured array with DM log contents.
         """
+        bytes = fh.read()
+        fh.close()
+        pattern = [
+            "(?P<timestamp>[0-9]{4}-[0-9]+-[0-9]+T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]+Z)[\s]*",
+            "(?P<xml>\<\?xml[\s\S]+?</event_message>)"
+        ]
+        xmlBlocks = re.findall("".join(pattern), bytes)
+        for tstamp,xmlBlock in xmlBlocks:
+            elMsg = etree.fromstring(xmlBlock)
+            category = elMsg.get("category")
+            instance = elMsg.get("instance")
+            messageType = elMsg.get("message_type")
+            timestamp = dateutil.parser.parse(elMsg.get("timestamp"))
+            version = int(elMsg.get("version"))
+
+            elCoreInfo = getChild(elMsg, "core_info")
+            id_dm = int(elCoreInfo.get("id"))
+
+            elMag = getChild(elCoreInfo, "mag")
+            magnitude = float(elMag.text)
+            magnitude_type = elMag.get("units")
+
+            latitude = float(getChild(elCoreInfo, "lat").text)
+            longitude = float(getChild(elCoreInfo, "lon").text)
+            depthKm = float(getChild(elCoreInfo, "depth").text)
+            originTime = dateutil.parser.parse(getChild(elCoreInfo, "orig_time").text)
+            likelihood = float(getChild(elCoreInfo, "likelihood").text)
+            numStations = int(getChild(elCoreInfo, "num_stations").text)
+
+            # :TODO: Add data to database
+            print id_dm, magnitude, timestamp
         return
         
 
+    def _getChild(el, name):
+        """Get child element 'name'. Raises IOError if more than one child element with name is found.
+
+        :type el: XML element
+        :param el: XML element
+
+        :type name: str
+        :param name: Name of child element.
+        """
+        elList = el.xpath(name)
+        if len(elList) != 1:
+            raise IOError("Expect one '%s' child element in XML element. Found %d '%s' elements in %s." % \
+                          (name, len(elList), el.tag))
+        return elList[0]
+
+
+    
 class DMLogASCII(object):
     """DM log fetcher and parser.
     """
