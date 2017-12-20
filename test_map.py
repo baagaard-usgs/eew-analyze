850672647025ff049811cab1ae4321c7e18be1fb
Brad Aagaard <baagaard@usgs.gov>
2017-12-20 10:50:42 -0800

    More work on setting up maps.

--- test_map.py
+++ test_map.py
@@ -3,15 +3,19 @@ import qgis.gui
 import PyQt4.QtCore
 import PyQt4.QtGui
 
+from osgeo import gdal, osr
+gdal.UseExceptions()
+
+
 standalone = True
 
 if standalone:
-    #qgis.core.QgsApplication.setPrefixPath("/usr", True)
     app = qgis.core.QgsApplication([], True)
+    qgis.core.QgsApplication.setPrefixPath("/Applications/QGIS.app/Contents/MacOS", True)
     app.initQgis()
 
 values = ["warning_time", "mmi_obs", "mmi_pred", "population_density",]
-renderValues = ["mmi_obs", "population_density"]
+renderValues = ["mmi_obs", "population_density", "basemap"]
 
 layers = {}
 for value in values:
@@ -22,7 +26,7 @@ for value in values:
     else:
         layer.setName(value)
         layers[value] = layer
-
+        
 layers["mmi_obs"].setDrawingStyle("SingleBandPsuedoColor")
 layers["mmi_obs"].loadNamedStyle("mmi_style.qml")
 
@@ -32,19 +36,50 @@ layers["mmi_pred"].loadNamedStyle("mmi_style.qml")
 layers["population_density"].setDrawingStyle("SingleBandGray")
 layers["population_density"].loadNamedStyle("pop_style.qml")
 
+layers["basemap"] = qgis.core.QgsRasterLayer("esri_streetmap.xml")
+layers["basemap"].setDrawingStyle('MultiBandSingleBandGray')
+layers["basemap"].renderer().setGrayBand(1)
+
 layerRegistry = qgis.core.QgsMapLayerRegistry.instance()
-layerRegistry.addMapLayers([layers[value] for value in values])
+#layerRegistry.addMapLayers([layers[value] for value in values])
+layerRegistry.addMapLayer(layers["basemap"])
+
+def contours_from_raster(filename, contourStart=1.5, contourInterval=0.5):
+    src = gdal.Open(filename, gdal.GA_ReadOnly)
+
+    dest = gdal.GetDriverByName("MEM").Create("temp", numLon, numLat, nbands, gdal.GDT_Float32)
+
+    driverMemory = ogr.GetDriverByName("MEMORY")
+    contourDataSrc = driverMemory.CreateDataSource("temp")
+    driverMemory.Open("temp", gdal.GA_Update)
+
+    contourLayer = contourDataSrc.CreateLayer("temp", src.GetSpatialRef(), populationLayerDefn.GetGeomType())
+    contourLayerDefn = popDensityLayer.GetLayerDefn()
+
+    contourLayer.CreateField(ogr.FieldDefn("id", ogr.OFInteger))
+    contourLayer.CreateField(ogr.FieldDefn("mmi_obs", ogr.OFTReal))
+    gdal.ContourGenerate(src.GetRasterBand(1), contourInterval, contourStart, 0, [], 0, 0, contourLayer, 0, 1)
+    del contourDataSrc
+
+    # Create QgsVectorLayer (memory) and transfer geometry
+    
+    
+    return contourLayer
+
+# contour mmi_obs
+mmiObsContours = contours_from_raster("./data//nc72923380/analysis_data-mmi_obs.vrt", 1.5, 0.5)
+
+import pdb
+pdb.set_trace()
 
 #    gdal.ContourGenerate(ds.GetRasterBand(1), 10, 0, [], 0, 0, ogr_lyr, 0, 1)
-
-
 
 
 
 #canvas = qgis.gui.QgsMapCanvas()
 #canvas.setCanvasColor(PyQt4.QtCore.Qt.white)
 #canvas.enableAntiAliasing(True)
-#canvas.setExtent(layers[renderLayers[0].extent())
+#canvas.setExtent(layers[renderValues[0]].extent())
 #canvas.setLayerSet([qgis.gui.QgsMapCanvasLayer(layers[value]) for value in renderValues])
 #
 #if standalone:
@@ -54,7 +89,7 @@ layerRegistry.addMapLayers([layers[value] for value in values])
 #renderer.setProjectionsEnabled(True)
 #renderer.setDestinationCrs(qgis.core.QgsCoordinateReferenceSystem("EPSG:3311"))
 #canvas.show()
-    
+
 image = PyQt4.QtGui.QImage(PyQt4.QtCore.QSize(1920, 1080), PyQt4.QtGui.QImage.Format_ARGB32_Premultiplied)
 image.fill(PyQt4.QtCore.Qt.white)
 
@@ -64,7 +99,8 @@ painter.setRenderHint(PyQt4.QtGui.QPainter.Antialiasing)
 
 renderer = qgis.core.QgsMapRenderer()
 
-renderer.setLayerSet([layers[value].id() for value in renderValues])
+#renderer.setLayerSet([layers[value].id() for value in renderValues])
+renderer.setLayerSet([layers["basemap"].id()])
 renderer.setProjectionsEnabled(True)
 renderer.setDestinationCrs(qgis.core.QgsCoordinateReferenceSystem("EPSG:3311"))
 
