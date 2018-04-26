c5a4824090e10f89c2aab0b5f5fce0a1f7f16aa2
Brad Aagaard <baagaard@usgs.gov>
2018-04-26 14:06:47 -0700

    Fix database locking issue when using multiprocessing.

--- eewperformance/analysisdb.py
+++ eewperformance/analysisdb.py
@@ -88,6 +88,32 @@ TABLES = [
 ]
 
 
+class Operation(object):
+    """Database operation object for minimizing locking behavior in database queries.
+
+    Based on Tranaction object in beets library
+    (https://github.com/beetbox/beets/blob/master/beets/dbcore/db.py)
+
+    """
+
+    def __init__(self, filename):
+        self.connection = sqlite3.connect(filename, timeout=20.0)
+        self.connection.row_factory = sqlite3.Row
+        self.cursor = self.connection.cursor()
+        return    
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.connection:
+            self.connection.commit()
+            self.connection.close()
+        self.connection = None
+        self.cursor = None
+        return
+
+
 class AnalysisData(object):
     """SQLite database with DM alerts and ComCat events.
     """
@@ -99,25 +125,31 @@ class AnalysisData(object):
         :param filename: Filename of SQLite database
 
         """
-        self.connection = sqlite3.connect(filename)
-        self.connection.row_factory = sqlite3.Row
-        self.cursor = self.connection.cursor()
+        self.filename = filename
+        
+        #self.connection = sqlite3.connect(filename, timeout=2.0)
+        #self.connection.row_factory = sqlite3.Row
+        #self.cursor = self.connection.cursor()
         return
 
+    def operation(self):
+        return Operation(self.filename)
+    
+    
     def init(self, key):
         """Create database.
         """
-        if key == "all":
-            for name, columns in TABLES[::-1]:
-                self.cursor.execute("DROP TABLE IF EXISTS {}".format(name))
-            for name,columns in TABLES:
-                self.cursor.execute("CREATE TABLE {name} ({fields})".format(name=name, fields=", ".join(columns)))
-        else:
-            for name, columns in TABLES:
-                if name == key:
-                    self.cursor.execute("DROP TABLE IF EXISTS {}".format(name))
-                    self.cursor.execute("CREATE TABLE {name} ({fields})".format(name=name, fields=", ".join(columns)))
-        self.connection.commit()
+        with self.operation() as op:
+            if key == "all":
+                for name, columns in TABLES[::-1]:
+                    op.cursor.execute("DROP TABLE IF EXISTS {}".format(name))
+                for name,columns in TABLES:
+                    op.cursor.execute("CREATE TABLE {name} ({fields})".format(name=name, fields=", ".join(columns)))
+            else:
+                for name, columns in TABLES:
+                    if name == key:
+                        op.cursor.execute("DROP TABLE IF EXISTS {}".format(name))
+                        op.cursor.execute("CREATE TABLE {name} ({fields})".format(name=name, fields=", ".join(columns)))
         return
 
     def add_alerts(self, alerts, replace=False):
@@ -282,19 +314,23 @@ class AnalysisData(object):
             "population_cost_perfecteew",
             "population_metric",
             )
-
+        NTRIES = 50
+        
         insertCols = ", ".join(COLUMNS)
         perfValues = [stats[col] for col in COLUMNS]
         perfCols = ",".join("?"*len(perfValues))
         cmd = "INSERT"
         if replace:
             cmd += " OR REPLACE"
-        try:
-            self.cursor.execute("{0} INTO performance({1}) VALUES({2})".format(cmd, insertCols, perfCols), perfValues)
-            self.connection.commit()
-        except sqlite3.IntegrityError as ex:
-            logging.getLogger(__name__).debug(str(ex))
-            logging.getLogger(__name__).debug(str(stats))
+
+        with self.operation() as op:
+            try:
+                op.cursor.execute("{0} INTO performance({1}) VALUES({2})".format(cmd, insertCols, perfCols), perfValues)
+            
+            except sqlite3.IntegrityError as ex:
+                logging.getLogger(__name__).debug(str(ex))
+                logging.getLogger(__name__).debug(str(stats))
+
         return
 
     def find_match(self, comcatId, server):
@@ -307,32 +343,33 @@ class AnalysisData(object):
         MAX_TIME_SECS = 15.0
         VS = 3.0e+3
 
-        self.cursor.execute("SELECT * FROM comcat_events WHERE event_id=?", (comcatId,))
-        event = self.cursor.fetchone()
+        with self.operation() as op:
+            op.cursor.execute("SELECT * FROM comcat_events WHERE event_id=?", (comcatId,))
+            event = op.cursor.fetchone()
 
-        lat = event["latitude"]
-        lon = event["longitude"]
-        ot = dateutil.parser.parse(event["origin_time"])
-        dt = datetime.timedelta(seconds=MAX_TIME_SECS)
-        conditions = [
-            "category=?",
-            "message_type=?",
-            "latitude BETWEEN ? AND ?",
-            "longitude BETWEEN ? AND ?",
-            "origin_time BETWEEN ? AND ?",
+            lat = event["latitude"]
+            lon = event["longitude"]
+            ot = dateutil.parser.parse(event["origin_time"])
+            dt = datetime.timedelta(seconds=MAX_TIME_SECS)
+            conditions = [
+                "category=?",
+                "message_type=?",
+                "latitude BETWEEN ? AND ?",
+                "longitude BETWEEN ? AND ?",
+                "origin_time BETWEEN ? AND ?",
             ]
-        values = (
-            "live",
-            "new",
-            lat-MAX_DISTANCE_DEG, lat+MAX_DISTANCE_DEG,
-            lon-MAX_DISTANCE_DEG, lon+MAX_DISTANCE_DEG,
-            ot-dt, ot+dt,
+            values = (
+                "live",
+                "new",
+                lat-MAX_DISTANCE_DEG, lat+MAX_DISTANCE_DEG,
+                lon-MAX_DISTANCE_DEG, lon+MAX_DISTANCE_DEG,
+                ot-dt, ot+dt,
             )
-        self.cursor.execute("SELECT * FROM eew_alerts WHERE " + " AND ".join(conditions), values)
-        alerts = self.cursor.fetchall()
+            op.cursor.execute("SELECT * FROM eew_alerts WHERE " + " AND ".join(conditions), values)
+            alerts = op.cursor.fetchall()
         if 0 == len(alerts):
             return None
-            
+        
         # Get closest alert, ignoring deleted alerts
         minDist = 1.0e+30
         alertMatch = None
@@ -378,8 +415,9 @@ class AnalysisData(object):
             alert["server"],
             timestamp, timestamp+datetime.timedelta(minutes=10.0),
             )
-        self.cursor.execute("SELECT * FROM eew_alerts WHERE " + " AND ".join(conditions), values)
-        alerts = self.cursor.fetchall()
+        with self.operation() as op:
+            op.cursor.execute("SELECT * FROM eew_alerts WHERE " + " AND ".join(conditions), values)
+            alerts = op.cursor.fetchall()
         return alerts
 
     def performance_stats(self, comcatId, server, gmpe, fragility):
@@ -397,32 +435,34 @@ class AnalysisData(object):
             gmpe,
             fragility,
             )
-        self.cursor.execute("SELECT * FROM performance WHERE " + " AND ".join(conditions) + " ORDER BY magnitude_threshold,mmi_threshold", values)
-        # :TODO: :KLUDGE: to get structured array
-        # Can we use PRAGMA TABLE_INFO to get column name and type (to map to numpy.dtype)?
-        dtype = [
-            ("comcat_id", "|S32"),
-            ("eew_server", "|S32"),
-            ("dm_id", "int32"),
-            ("dm_timestamp", "|S32"),
-            ("gmpe", "|S32"),
-            ("fragility", "|S32"),
-            ("magnitude_threshold", "float32"),
-            ("mmi_threshold", "float32"),
-            ("area_damage", "float32"),
-            ("area_alert", "float32"),
-            ("area_cost_eew", "float32"),
-            ("area_cost_noeew", "float32"),
-            ("area_cost_perfecteew", "float32"),
-            ("area_metric", "float32"),
-            ("population_damage", "float32"),
-            ("population_alert", "float32"),
-            ("population_cost_eew", "float32"),
-            ("population_cost_noeew", "float32"),
-            ("population_cost_perfecteew", "float32"),
-            ("population_metric", "float32"),
+        with self.operation() as op:
+            op.cursor.execute("SELECT * FROM performance WHERE " + " AND ".join(conditions) + " ORDER BY magnitude_threshold,mmi_threshold", values)
+            # :TODO: :KLUDGE: to get structured array
+            # Can we use PRAGMA TABLE_INFO to get column name and type (to map to numpy.dtype)?
+            dtype = [
+                ("comcat_id", "|S32"),
+                ("eew_server", "|S32"),
+                ("dm_id", "int32"),
+                ("dm_timestamp", "|S32"),
+                ("gmpe", "|S32"),
+                ("fragility", "|S32"),
+                ("magnitude_threshold", "float32"),
+                ("mmi_threshold", "float32"),
+                ("area_damage", "float32"),
+                ("area_alert", "float32"),
+                ("area_cost_eew", "float32"),
+                ("area_cost_noeew", "float32"),
+                ("area_cost_perfecteew", "float32"),
+                ("area_metric", "float32"),
+                ("population_damage", "float32"),
+                ("population_alert", "float32"),
+                ("population_cost_eew", "float32"),
+                ("population_cost_noeew", "float32"),
+                ("population_cost_perfecteew", "float32"),
+                ("population_metric", "float32"),
             ]
-        results = self.cursor.fetchall()
+            results = op.cursor.fetchall()
+
         nrows = len(results)
         stats = numpy.zeros(nrows, dtype=dtype)
         for iresult, result in enumerate(results):
@@ -437,8 +477,9 @@ class AnalysisData(object):
 
         :returns: Most recent alert for server in database.
         """
-        self.cursor.execute("SELECT * from eew_alerts ORDER BY date(timestamp) DESC LIMIT 1")
-        alert = self.cursor.fetchone()
+        with self.operation() as op:
+            op.cursor.execute("SELECT * from eew_alerts ORDER BY date(timestamp) DESC LIMIT 1")
+            alert = op.cursor.fetchone()
         return alert
     
     def comcat_event(self, comcatId):
@@ -447,8 +488,9 @@ class AnalysisData(object):
         :type comcatId: str
         :param comcatId: ComCat event id
         """
-        self.cursor.execute("SELECT * FROM comcat_events WHERE event_id=?", (comcatId,))
-        event = self.cursor.fetchone()
+        with self.operation() as op:
+            op.cursor.execute("SELECT * FROM comcat_events WHERE event_id=?", (comcatId,))
+            event = op.cursor.fetchone()
         return event
 
     def comcat_shakemap(self, comcatId):
@@ -457,25 +499,27 @@ class AnalysisData(object):
         :type comcatId: str
         :param comcatId: ComCat event id
         """
-        self.cursor.execute("SELECT * FROM comcat_shakemaps WHERE event_id=?", (comcatId,))
-        shakemap = self.cursor.fetchone()
+        with self.operation() as op:
+            op.cursor.execute("SELECT * FROM comcat_shakemaps WHERE event_id=?", (comcatId,))
+            shakemap = op.cursor.fetchone()
         return shakemap
     
     def tables_info(self):
         """Returns string with database summary.
         """
         sout = ""
-        for name,columns in TABLES:
-            self.cursor.execute("PRAGMA TABLE_INFO({})".format(name))
-            info = self.cursor.fetchall()
-            self.cursor.execute("SELECT COUNT(*) FROM {}".format(name))
-            nrows = self.cursor.fetchall()[0][0]
+        with self.operation() as op:
+            for name,columns in TABLES:
+                op.cursor.execute("PRAGMA TABLE_INFO({})".format(name))
+                info = op.cursor.fetchall()
+                op.cursor.execute("SELECT COUNT(*) FROM {}".format(name))
+                nrows = op.cursor.fetchall()[0][0]
 
-            sout += "Table {}\n".format(name)
-            sout += "  Columns\n"
-            for column in info:
-                sout += "    {name:16} {type:16}\n".format(name=column[1], type=column[2])
-            sout += "  Number of rows: {}\n".format(nrows)
+                sout += "Table {}\n".format(name)
+                sout += "  Columns\n"
+                for column in info:
+                    sout += "    {name:16} {type:16}\n".format(name=column[1], type=column[2])
+                sout += "  Number of rows: {}\n".format(nrows)
         return sout
 
     def summary(self):
@@ -483,38 +527,42 @@ class AnalysisData(object):
         """
         sout = ""
 
-        # Comcat events
-        sout += "\nComCat Events\n"
-        self.cursor.execute("SELECT * FROM comcat_events ORDER BY origin_time")
-        rows = self.cursor.fetchall()
-        for row in rows:
-            ot = dateutil.parser.parse(row["origin_time"])
-            sout += "{row[event_id]} {row[longitude]:9.4f} {row[latitude]:8.4f} {row[depth_km]:4.1f} {ot:%Y-%m-%dT%H:%M} {row[magnitude_type]:3s}{row[magnitude]:.2f} {row[description]}\n".format(row=row, ot=ot)
-
-        # Comcat Shakemap
-        sout += "\nShakeMap Info\n"
-        self.cursor.execute("SELECT * FROM comcat_shakemaps ORDER BY event_id")
-        rows = self.cursor.fetchall()
-        for row in rows:
-            event = self.comcat_event(row["event_id"])
-            sout += "{row[event_id]} {event[magnitude_type]:3s}{event[magnitude]:.2f} {row[mmi_max]:3.1f} {row[pga_max]:6.2f}%g {row[pgv_max]:5.1f}cm/s {row[mmi_bias]:5.2f} {row[pga_bias]:5.2f} {row[pgv_bias]:5.2f} {row[gmpe]} {row[pgm2mi]} v{row[software_version]} {event[description]}\n".format(row=row, event=event)
-            
-        # Alerts
-
-        # Performance
-        sout += "\nPerformance Data\n"
-        self.cursor.execute("SELECT * FROM performance ORDER BY comcat_id,fragility,gmpe,magnitude_threshold,mmi_threshold")
-        rows = self.cursor.fetchall()
-        for row in rows:
-            event = self.comcat_event(row["comcat_id"])
-            sout += "{row[comcat_id]} {event[magnitude_type]:3s}{event[magnitude]:.2f} {row[gmpe]} {row[fragility]} {row[magnitude_threshold]:3.1f} {row[mmi_threshold]:3.1f} {row[area_metric]:6.2f} {row[population_metric]:6.2f} {event[description]}\n".format(row=row, event=event)
+        with self.operation as op:
+        
+            # Comcat events
+            sout += "\nComCat Events\n"
+            op.cursor.execute("SELECT * FROM comcat_events ORDER BY origin_time")
+            rows = op.cursor.fetchall()
+            for row in rows:
+                ot = dateutil.parser.parse(row["origin_time"])
+                sout += "{row[event_id]} {row[longitude]:9.4f} {row[latitude]:8.4f} {row[depth_km]:4.1f} {ot:%Y-%m-%dT%H:%M} {row[magnitude_type]:3s}{row[magnitude]:.2f} {row[description]}\n".format(row=row, ot=ot)
+    
+            # Comcat Shakemap
+            sout += "\nShakeMap Info\n"
+            op.cursor.execute("SELECT * FROM comcat_shakemaps ORDER BY event_id")
+            rows = op.cursor.fetchall()
+            for row in rows:
+                event = self.comcat_event(row["event_id"])
+                sout += "{row[event_id]} {event[magnitude_type]:3s}{event[magnitude]:.2f} {row[mmi_max]:3.1f} {row[pga_max]:6.2f}%g {row[pgv_max]:5.1f}cm/s {row[mmi_bias]:5.2f} {row[pga_bias]:5.2f} {row[pgv_bias]:5.2f} {row[gmpe]} {row[pgm2mi]} v{row[software_version]} {event[description]}\n".format(row=row, event=event)
+                
+            # Alerts
+    
+            # Performance
+            sout += "\nPerformance Data\n"
+            op.cursor.execute("SELECT * FROM performance ORDER BY comcat_id,fragility,gmpe,magnitude_threshold,mmi_threshold")
+            rows = op.cursor.fetchall()
+            for row in rows:
+                event = self.comcat_event(row["comcat_id"])
+                sout += "{row[comcat_id]} {event[magnitude_type]:3s}{event[magnitude]:.2f} {row[gmpe]} {row[fragility]} {row[magnitude_threshold]:3.1f} {row[mmi_threshold]:3.1f} {row[area_metric]:6.2f} {row[population_metric]:6.2f} {event[description]}\n".format(row=row, event=event)
         return sout
 
     def show_matches(self, server):
         """Show matches.
         """
-        self.cursor.execute("SELECT * from comcat_events ORDER BY event_id")
-        events = self.cursor.fetchall()
+        with self.operation() as op:
+            op.cursor.execute("SELECT * from comcat_events ORDER BY event_id")
+            events = op.cursor.fetchall()
+            
         for event in events:
             alert = self.find_match(event["event_id"], server)
             if alert:
