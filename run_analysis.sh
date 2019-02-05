ec6c5f97c5319a721744f6de909c816141f67715
Brad Aagaard <baagaard@usgs.gov>
2019-02-05 14:14:15 -0800

    Fix typos.

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+./downloader.py --db-init=performance
+
 eqset="eqsets/sanfrancisco.cfg,eqsets/losangeles.cfg"
 
 # FearAvoidanceLinear
 ./analyzer.py --config=${eqset} --num-threads=16 --optimize-events --plot-event-maps=all --plot-event-figures=all
-./analyzer.py --config=${eqset},catalog_magnitude.cfg --num-threads=16--optimize-events --plot-event-maps=alert
+./analyzer.py --config=${eqset},catalog_magnitude.cfg --num-threads=16 --optimize-events --plot-event-maps=alert
 ./analyzer.py --config=${eqset},catalog_magnitude_bias.cfg --num-threads=16 --optimize-events --plot-event-maps=alert
-./analyzer.py --config=${eqset} --plot-summary-figures=all --plot-summary-maps=all --generate-report && mv report.pdf report_LA+SF-FearAvoidanceLinear.pdf
+./analyzer.py --config=${eqset} --plot-summary-figures=all --plot-summary-maps=all --generate-report=full && mv report.pdf report_LA+SF-FearAvoidanceLinear.pdf
 
 # FearAvoidanceStep
-costfns=fragility_fearavoidance_step.cfg
+costfns="fragility_fearavoidance_step.cfg"
 ./analyzer.py --config=${eqset},${costfns} --num-threads=16 --optimize-events --plot-event-maps=all --plot-event-figures=all
-./analyzer.py --config=${eqset},${costfns},catalog_magnitude.cfg --num-threads=16--optimize-events --plot-event-maps=alert
+./analyzer.py --config=${eqset},${costfns},catalog_magnitude.cfg --num-threads=16 --optimize-events --plot-event-maps=alert
 ./analyzer.py --config=${eqset},${costfns},catalog_magnitude_bias.cfg --num-threads=16 --optimize-events --plot-event-maps=alert
-./analyzer.py --config=${eqset},${costfns} --plot-summary-figures=all --plot-summary-maps=all --generate-report && mv report.pdf report_LA+SF-FearAvoidanceStep.pdf
+./analyzer.py --config=${eqset},${costfns} --plot-summary-figures=all --plot-summary-maps=all --generate-report=full && mv report.pdf report_LA+SF-FearAvoidanceStep.pdf
 
 # FearAvoidanceSigmoid
-costfns=fragility_fearavoidance_sigmoid.cfg
+costfns="fragility_fearavoidance_sigmoid.cfg"
 ./analyzer.py --config=${eqset},${costfns} --num-threads=16 --optimize-events --plot-event-maps=all --plot-event-figures=all
-./analyzer.py --config=${eqset},${costfns},catalog_magnitude.cfg --num-threads=16--optimize-events --plot-event-maps=alert
+./analyzer.py --config=${eqset},${costfns},catalog_magnitude.cfg --num-threads=16 --optimize-events --plot-event-maps=alert
 ./analyzer.py --config=${eqset},${costfns},catalog_magnitude_bias.cfg --num-threads=16 --optimize-events --plot-event-maps=alert
-./analyzer.py --config=${eqset},${costfns} --plot-summary-figures=all --plot-summary-maps=all --generate-report && mv report.pdf report_LA+SF-FearAvoidanceSigmoid.pdf
+./analyzer.py --config=${eqset},${costfns} --plot-summary-figures=all --plot-summary-maps=all --generate-report=full && mv report.pdf report_LA+SF-FearAvoidanceSigmoid.pdf
