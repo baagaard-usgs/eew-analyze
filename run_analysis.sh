c8ff892862da776ecb818961081239245d520192
Brad Aagaard <baagaard@usgs.gov>
2019-02-07 16:04:17 -0800

    Update analysis batch script.

--- run_analysis.sh
+++ run_analysis.sh
@@ -24,3 +24,24 @@ costfns="fragility_fearavoidance_sigmoid.cfg"
 ./analyzer.py --config=${eqset},${costfns},catalog_magnitude.cfg --num-threads=16 --optimize-events --plot-event-maps=alert
 ./analyzer.py --config=${eqset},${costfns},catalog_magnitude_bias.cfg --num-threads=16 --optimize-events --plot-event-maps=alert
 ./analyzer.py --config=${eqset},${costfns} --plot-summary-figures=all --plot-summary-maps=all --generate-report=full && mv -f report.pdf report_LA+SF-FearAvoidanceSigmoid.pdf
+
+# FearAvoidanceLinearR20
+costfns="fragility_fearavoidance_r20.cfg"
+./analyzer.py --config=${eqset},${costfns} --num-threads=16 --optimize-events --plot-event-maps=all --plot-event-figures=all
+./analyzer.py --config=${eqset},${costfns},catalog_magnitude.cfg --num-threads=16 --optimize-events --plot-event-maps=alert
+./analyzer.py --config=${eqset},${costfns},catalog_magnitude_bias.cfg --num-threads=16 --optimize-events --plot-event-maps=alert
+./analyzer.py --config=${eqset},${costfns} --plot-summary-figures=all --plot-summary-maps=all --generate-report=full && mv -f report.pdf report_LA+SF-FearAvoidanceLinearR20.pdf
+
+# InjuryLinear
+costfns="fragility_injury.cfg"
+./analyzer.py --config=${eqset},${costfns} --num-threads=16 --optimize-events --plot-event-maps=all --plot-event-figures=all
+./analyzer.py --config=${eqset},${costfns},catalog_magnitude.cfg --num-threads=16 --optimize-events --plot-event-maps=alert
+./analyzer.py --config=${eqset},${costfns},catalog_magnitude_bias.cfg --num-threads=16 --optimize-events --plot-event-maps=alert
+./analyzer.py --config=${eqset},${costfns} --plot-summary-figures=all --plot-summary-maps=all --generate-report=full && mv -f report.pdf report_LA+SF-InjuryLinear.pdf
+
+# InjuryLinearW2
+costfns="fragility_injury_w2.cfg"
+./analyzer.py --config=${eqset},${costfns} --num-threads=16 --optimize-events --plot-event-maps=all --plot-event-figures=all
+./analyzer.py --config=${eqset},${costfns},catalog_magnitude.cfg --num-threads=16 --optimize-events --plot-event-maps=alert
+./analyzer.py --config=${eqset},${costfns},catalog_magnitude_bias.cfg --num-threads=16 --optimize-events --plot-event-maps=alert
+./analyzer.py --config=${eqset},${costfns} --plot-summary-figures=all --plot-summary-maps=all --generate-report=full && mv -f report.pdf report_LA+SF-InjuryLinearW2.pdf
