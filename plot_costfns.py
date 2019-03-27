474e99ca604b22e717cfc74987951dd980c15298
Brad Aagaard <baagaard@usgs.gov>
2019-03-27 13:29:12 -0700

    Plot cost functions on same axes.

--- plot_costfns.py
+++ plot_costfns.py
@@ -17,36 +17,35 @@ COSTFNS = {
         ("Linear", "fragility_fearavoidance.cfg"),
         ("Sigmoid", "fragility_fearavoidance_sigmoid.cfg"),
         ("Step", "fragility_fearavoidance_step.cfg"),
-        ("Linear, r=20", "fragility_fearavoidance_r20.cfg"),
+        #("Linear, r=20", "fragility_fearavoidance_r20.cfg"),
     ],
     "Injury": [
         ("LinearW4", "fragility_injury.cfg"),
         ("LinearW2", "fragility_injury_w2.cfg"),
     ]
 }
-FIG_SIZE = (7.0, 8.5)
-MARGINS = ((0.6, 0.8, 0.1), (0.5, 0.8, 0.6))
+FIG_SIZE = (7.5, 3.5)
+MARGINS = ((0.6, 0.8, 0.1), (0.5, 0.8, 0.3))
    
+pyplot.style.use("size-presentation")
+pyplot.style.use("color-darkbg")
+
+figure = pyplot.figure(figsize=FIG_SIZE)
+nrows = 1
+ncols = 2
+ax_factory = matplotlib_extras.axes.RectFactory(figure, nrows=nrows, ncols=ncols, margins=MARGINS)
 
 mmi = numpy.arange(1.0, 9.01, 0.01)
 
-pyplot.style.use("size-presentation")
-pyplot.style.use("color-lightbg")
-
-figure = pyplot.figure(figsize=FIG_SIZE)
-nrows = max(len(COSTFNS["Fear"]), len(COSTFNS["Injury"]))
-
-ax_factory = matplotlib_extras.axes.RectFactory(figure, nrows=nrows, ncols=2, margins=MARGINS)
-
-
 col = 1
+row = 1
 for category in ["Fear", "Injury"]:
 
-    row = 1
+    ax = figure.add_axes(ax_factory.rect(row=row, col=col))
+    plot_action = True
+    
     for label, costfn_config in COSTFNS[category]:
     
-        ax = figure.add_axes(ax_factory.rect(row=row, col=col))
-
         config = configparser.SafeConfigParser()
         config.read(costfn_config)
 
@@ -60,18 +59,18 @@ for category in ["Fear", "Injury"]:
         cost_damage = fragility.cost_damage(mmi)
         cost_action = fragility.cost_action(mmi)
 
-        ax.plot(mmi, cost_action, label="Action")
-        ax.plot(mmi, cost_damage, label="Damage")
+        if plot_action:
+            ax.plot(mmi, cost_action, label="Action")
+            plot_action = False
+        ax.plot(mmi, cost_damage, label="{} Damage".format(label))
 
-        ax.set_title(label)
         ax.spines["top"].set_visible(False)
         ax.spines["right"].set_visible(False)
 
         ax.xaxis.set_major_locator(ticker.MultipleLocator(1.0))
         ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.5))
         ax.set_xlim(1.0, 9.0)
-        if row == len(COSTFNS[category]):
-            ax.set_xlabel("MMI")
+        ax.set_xlabel("MMI")
 
         ax.yaxis.set_major_locator(ticker.MultipleLocator(0.2))
         ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.1))
@@ -81,10 +80,7 @@ for category in ["Fear", "Injury"]:
         if row == 1 and col == 1:
             ax.legend(loc="center right")
         
-        row += 1
-
-    ax.text(0.25+0.5*(col-1), 0.98, "{} Reduction".format(category),
-            transform=figure.transFigure, ha="center", weight="bold")
+    ax.set_title("{} Reduction".format(category), ha="center", weight="bold")
     col += 1
         
 figure.savefig("costfns.pdf")
